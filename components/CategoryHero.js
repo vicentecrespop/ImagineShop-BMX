3f49787b4255c0c9e3cdb52f8de4434a88b52490
vicentecrespop <vicentecrespo45@gmail.com>
2023-03-28 12:26:42 -0300

    compontens

--- components/CategoryHero.js
+++ components/CategoryHero.js
@@ -1,4 +1,10 @@
+import Image from "next/image"
 import Link from "next/link"
+
+import fundoBikes from '@/public/img/fundo-bikes.png'
+import fundoPecas from '@/public/img/fundo-pecas.png'
+import fundoQuadros from '@/public/img/fundo-quadros.png'
+import fundoRoupas from '@/public/img/fundo-roupas.png'
 
 export default function CategoryHero() {
     return (
@@ -8,7 +14,7 @@ export default function CategoryHero() {
                     <div className="row">
                         <div className="col-6 col-lg-3">
                             <div className="category-item position-relative border border-primary d-flex justify-content-center align-items-end">
-                                <img className="w-100" src="https://cdn.shopify.com/s/files/1/0062/5708/2479/files/Category_Collection_Block_Bikes_656c6e50-f08e-4e49-9405-abdc28fa396a_360x.jpg?v=1673259370" alt="Bikes"/>
+                                <Image className="w-100 h-100" src={fundoBikes} alt="Bikes"/>
                                 <div className="category-item--info position-absolute bg-dark bg-opacity-75 text-center fw-bold text-white d-flex flex-column">
                                     <span>BIKES</span>
                                     <span className="my-2">ATÉ 50% OFF</span>
@@ -18,7 +24,7 @@ export default function CategoryHero() {
                         </div>
                         <div className="col-6 col-lg-3">
                             <div className="category-item position-relative border border-primary d-flex justify-content-center align-items-end">
-                                <img className="w-100" src="https://cdn.shopify.com/s/files/1/0062/5708/2479/files/Category_Collection_Block_Parts_92beabf1-a021-421d-823d-b34e12ae8456_400x.jpg?v=1673259383" alt="Peças"/>
+                                <Image className="w-100 h-100" src={fundoPecas} alt="Peças"/>
                                 <div className="category-item--info position-absolute bg-dark bg-opacity-75 text-center fw-bold text-white d-flex flex-column ">
                                     <span>PEÇAS</span>
                                     <span className="my-2">ATÉ 50% OFF</span>
@@ -28,7 +34,7 @@ export default function CategoryHero() {
                         </div>
                         <div className="col-6 col-lg-3">
                             <div className="category-item position-relative border border-primary d-flex justify-content-center align-items-end">
-                                <img className="w-100" src="https://cdn.shopify.com/s/files/1/0062/5708/2479/files/Category_Collection_Block_Frames_174d16ac-f68d-47ec-8066-682b1444be57_360x.jpg?v=1673259384" alt="Quadros"/>
+                                <Image className="w-100 h-100" src={fundoQuadros} alt="Quadros"/>
                                 <div className="category-item--info position-absolute bg-dark bg-opacity-75 text-center fw-bold text-white d-flex flex-column ">
                                     <span>QUADROS</span>
                                     <span className="my-2">ATÉ 50% OFF</span>
@@ -38,7 +44,7 @@ export default function CategoryHero() {
                         </div>
                         <div className="col-6 col-lg-3">
                             <div className="category-item position-relative border border-primary d-flex justify-content-center align-items-end">
-                                <img className="w-100" src="https://cdn.shopify.com/s/files/1/0062/5708/2479/files/Category_Collection_Block_Softgoods_ce69286b-6dec-4a6c-954d-6605fa295ae2_360x.jpg?v=1673259383" alt="Roupas"/>
+                                <Image className="w-100 h-100" src={fundoRoupas} alt="Roupas"/>
                                 <div className="category-item--info position-absolute bg-dark bg-opacity-75 text-center fw-bold text-white d-flex flex-column ">
                                     <span>ROUPAS</span>
                                     <span className="my-2">ATÉ 50% OFF</span>
