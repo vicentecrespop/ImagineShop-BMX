d8389a9bad23a239ea7dc2bdb313ce0404a869ad
vicentecrespop <vicentecrespo45@gmail.com>
2023-03-28 10:19:43 -0300

    Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -34,14 +34,6 @@ export default function Navbar() {
                     <div className="col-9">
                         <div className="row d-flex align-items-center justify-content-end">
                             <div className="col-12 d-flex justify-content-end">    
-                                {/* <Link href="conta" className="ms-4 d-flex align-items-center text-decoration-none text-black">
-                                    <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
-                </svg>
-                                    </span>
-                                    <span className="mx-2 text-uppercase fw-bold">Minha conta</span>
-                                </Link> */}
                                 <Link href="carrinho" className="ms-4 d-flex align-items-center text-decoration-none text-black">
                                     <div className="position-relative">
                                         {totalProducts > 0 && (
